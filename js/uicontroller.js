c6b7b065b8c3241d8b5dcbc30f95cf90ce438d19
Gel-de-lune <mel_force10@yahoo.co.jp>
2021-12-03 16:28:42 +0900

    close #9
    Organize DOM objects finished.

--- js/uicontroller.js
+++ js/uicontroller.js
@@ -21,70 +21,107 @@ class UiController {
       // Select the first file
       this.filelist.value = event.target.files[0].name;
     });
+    // Input
+    this.fader_a_pitch = document.getElementsByTagName("input")[0];
+    this.radio_a_pitch_ratio_8 = document.getElementsByTagName("input")[1];
+    this.radio_a_pitch_ratio_16 = document.getElementsByTagName("input")[2];
+    this.radio_a_pitch_ratio_32 = document.getElementsByTagName("input")[3];
+    this.radio_a_pitch_ratio_64 = document.getElementsByTagName("input")[4];
+    this.a_gain = document.getElementsByTagName("input")[5];
+    this.eq_a_hi = document.getElementsByTagName("input")[6];
+    this.eq_a_mid = document.getElementsByTagName("input")[7];
+    this.eq_a_lo = document.getElementsByTagName("input")[8];
+    this.chkbox_a_monitor = document.getElementsByTagName("input")[9];
+    this.fader_a_input = document.getElementsByTagName("input")[10];
+    this.file_browse = document.getElementsByTagName("input")[11];
+    this.volume_master = document.getElementsByTagName("input")[12];
+    this.volume_monitor = document.getElementsByTagName("input")[13];
+    this.b_gain = document.getElementsByTagName("input")[14];
+    this.eq_b_hi = document.getElementsByTagName("input")[15];
+    this.eq_b_mid = document.getElementsByTagName("input")[16];
+    this.eq_b_lo = document.getElementsByTagName("input")[17];
+    this.chkbox_b_monitor = document.getElementsByTagName("input")[18];
+    this.fader_b_input = document.getElementsByTagName("input")[19];
+    this.fader_cross = document.getElementsByTagName("input")[20];
+    this.fader_b_pitch = document.getElementsByTagName("input")[21];
+    this.radio_b_pitch_ratio_8 = document.getElementsByTagName("input")[22];
+    this.radio_b_pitch_ratio_16 = document.getElementsByTagName("input")[23];
+    this.radio_b_pitch_ratio_32 = document.getElementsByTagName("input")[24];
+    this.radio_b_pitch_ratio_64 = document.getElementsByTagName("input")[25];
+    // Button
+    this.btn_a_reset = document.getElementsByTagName("button")[0];
+    this.btn_a_cue = document.getElementsByTagName("button")[1];
+    this.btn_a_play = document.getElementsByTagName("button")[2];
+    this.btn_a_pad1 = document.getElementsByTagName("button")[3];
+    this.btn_a_pad2 = document.getElementsByTagName("button")[4];
+    this.btn_a_pad3 = document.getElementsByTagName("button")[5];
+    this.btn_a_pad4 = document.getElementsByTagName("button")[6];
+    this.btn_a_pad5 = document.getElementsByTagName("button")[7];
+    this.btn_a_pad6 = document.getElementsByTagName("button")[8];
+    this.btn_a_pad7 = document.getElementsByTagName("button")[9];
+    this.btn_a_pad8 = document.getElementsByTagName("button")[10];
+    this.btn_a_load = document.getElementsByTagName("button")[11];
+    this.btn_file_browse = document.getElementsByTagName("button")[12];
+    this.btn_b_load = document.getElementsByTagName("button")[13];
+    this.btn_b_reset = document.getElementsByTagName("button")[14];
+    this.btn_b_cue = document.getElementsByTagName("button")[15];
+    this.btn_b_play = document.getElementsByTagName("button")[16];
+    this.btn_b_pad1 = document.getElementsByTagName("button")[17];
+    this.btn_b_pad2 = document.getElementsByTagName("button")[18];
+    this.btn_b_pad3 = document.getElementsByTagName("button")[19];
+    this.btn_b_pad4 = document.getElementsByTagName("button")[20];
+    this.btn_b_pad5 = document.getElementsByTagName("button")[21];
+    this.btn_b_pad6 = document.getElementsByTagName("button")[22];
+    this.btn_b_pad7 = document.getElementsByTagName("button")[23];
+    this.btn_b_pad8 = document.getElementsByTagName("button")[24];
+
+
     // Cross Fader
-    this.crossfader = document.getElementsByTagName("input")[20];
-    this.crossfader.addEventListener("change", (event) => { this.audioprocess.onCrossFader(event.target.value); });
+    this.fader_cross.addEventListener("change", (event) => { this.audioprocess.onCrossFader(event.target.value); });
     // Master Volume
-    this.mastervolume = document.getElementsByTagName("input")[12];
-    this.mastervolume.addEventListener("change", (event) => { this.audioprocess.onMasterVolume(event.target.value); });
+    this.volume_master.addEventListener("change", (event) => { this.audioprocess.onMasterVolume(event.target.value); });
 
     // A audio
     this.a_audio = document.getElementsByTagName("audio")[0];
     // Fader A pitch
-    this.fader_a_pitch = document.getElementsByTagName("input")[0];
     this.fader_a_pitch.addEventListener("change", (event) => { this.onFaderAPitch(event.target.value); });
-    const reset_a_btn = document.getElementsByTagName("button")[0];
-    reset_a_btn.addEventListener("click", (event) =>{ this.fader_a_pitch.value = 64; this.onFaderAPitch(64);});
+    // Reset A pitch button
+    this.btn_a_reset.addEventListener("click", (event) =>{ this.fader_a_pitch.value = 64; this.onFaderAPitch(64);});
     // Load A button
-    this.load_a_btn = document.getElementsByTagName("button")[11];
-    this.load_a_btn.addEventListener("click", (event) =>{ this.onLoadADeck(event); });
+    this.btn_a_load.addEventListener("click", (event) =>{ this.onLoadADeck(event); });
     // A gain/trim
-    this.a_gain = document.getElementsByTagName("input")[5];
     this.a_gain.addEventListener("change", (event) => { this.audioprocess.onAGain(event.target.value); });
     // Fader A input
-    this.fader_a_input = document.getElementsByTagName("input")[10];
     this.fader_a_input.addEventListener("change", (event) => { this.audioprocess.onFaderAInput(event.target.value); });    
     // Equalizer A high
-    this.eq_a_hi = document.getElementsByTagName("input")[6];
     this.eq_a_hi.addEventListener("change", (event) => { this.audioprocess.onEqualizerAHigh(event.target.value); });
     // Equalizer A middle
-    this.eq_a_mid = document.getElementsByTagName("input")[7];
     this.eq_a_mid.addEventListener("change", (event) => { this.audioprocess.onEqualizerAMiddle(event.target.value); });
     // Equalizer A low
-    this.eq_a_lo = document.getElementsByTagName("input")[8];
     this.eq_a_lo.addEventListener("change", (event) => { this.audioprocess.onEqualizerALow(event.target.value); });
     // Play A button
-    this.play_a_btn = document.getElementsByTagName("button")[2];
-    this.play_a_btn.addEventListener("click", (event) =>{ this.onClickAPlay(event); });
+    this.btn_a_play.addEventListener("click", (event) =>{ this.onClickAPlay(event); });
 
     // B audio
     this.b_audio = document.getElementsByTagName("audio")[1];
     // Fader B pitch
-    this.fader_b_pitch = document.getElementsByTagName("input")[21];
     this.fader_b_pitch.addEventListener("change", (event) => { this.onFaderBPitch(event.target.value); });
-    const reset_b_btn = document.getElementsByTagName("button")[14];
-    reset_b_btn.addEventListener("click", (event) =>{ this.fader_b_pitch.value = 64; this.onFaderBPitch(64);})
+    // Reset B pitch button
+    this.btn_b_reset.addEventListener("click", (event) =>{ this.fader_b_pitch.value = 64; this.onFaderBPitch(64);})
     // Load B button
-    this.load_b_btn = document.getElementsByTagName("button")[13];
-    this.load_b_btn.addEventListener("click", (event) =>{ this.onLoadBDeck(event); });
+    this.btn_b_load.addEventListener("click", (event) =>{ this.onLoadBDeck(event); });
     // B gain/trim
-    this.b_gain = document.getElementsByTagName("input")[14];
     this.b_gain.addEventListener("change", (event) => { this.audioprocess.onBGain(event.target.value); });
     // Fader B input
-    this.fader_b_input = document.getElementsByTagName("input")[19];
     this.fader_b_input.addEventListener("change", (event) => { this.audioprocess.onFaderBInput(event.target.value); });    
     // Equalizer B high
-    this.eq_b_hi = document.getElementsByTagName("input")[15];
     this.eq_b_hi.addEventListener("change", (event) => { this.audioprocess.onEqualizerBHigh(event.target.value); });
     // Equalizer B middle
-    this.eq_b_mid = document.getElementsByTagName("input")[16];
     this.eq_b_mid.addEventListener("change", (event) => { this.audioprocess.onEqualizerBMiddle(event.target.value); });
     // Equalizer B low
-    this.eq_b_lo = document.getElementsByTagName("input")[17];
     this.eq_b_lo.addEventListener("change", (event) => { this.audioprocess.onEqualizerBLow(event.target.value); });
     // Play B button
-    this.play_b_btn = document.getElementsByTagName("button")[16];
-    this.play_b_btn.addEventListener("click", (event) =>{ this.onClickBPlay(event); });
+    this.btn_b_play.addEventListener("click", (event) =>{ this.onClickBPlay(event); });
   }
 
   audioprocess;
